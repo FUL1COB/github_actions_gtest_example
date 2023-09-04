0655beb17368c9e0a3773c170a40558cfeec01f4
FUL1COB <144001286+FUL1COB@users.noreply.github.com>
2023-09-04 15:41:48 +0530

    Update StringCalculator.cpp

--- test/StringCalculator.cpp
+++ test/StringCalculator.cpp
@@ -93,14 +93,8 @@ TEST(string_calculator_add_When_Passed_A_Single_Number, returns_0_for_empty_stri
     StringCalculator objUnderTest;
     string input = "";
     int expectedValue = 0;
-
- 
-
     //Act
 int actualValue=objUnderTest.Add(input);
-
- 
-
      //Assert - Fatal
 ASSERT_EQ(actualValue, expectedValue);
 }
